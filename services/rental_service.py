23af624bff0d8d5cd845d84850f96b929ab31429
Luis Neira <lneira41086@gmail.com>
2025-03-30 22:50:54 -0400

    document services

--- services/rental_service.py
+++ services/rental_service.py
@@ -1,31 +1,51 @@
+"""
+RentalService Module
+
+This module defines the `RentalService` class, which provides methods for managing rental listings. 
+It interacts with a datastore to perform CRUD operations on rental properties.
+"""
+
 from db import store
 
 
 class RentalService:
+    """ A service class for managing rental listings. """
+
     def __init__(self):
+        """ Initializes the RentalService with rental data from the datastore. """
+
         self.rentals = store["rentals"]
 
     def find_by_property_type(self, type):
-        res = [
+        """ Finds all rentals that match the given property type. """
+
+        return [
             r for r in self.rentals if r["property_type"] == type
         ]
-        return res
 
     def find_by_id(self, data, search_id):
+        """ Finds a rental by its unique ID within a given dataset. """
+
         for i, item in enumerate(data):
             if item['id'] == search_id:
                 return (item, i)
         return None
 
     def get_all_rentals(self):
+        """ Retrieves all rentals from the datastore. """
+
         return self.rentals
 
     def add_rental(self, payload):
+        """ Adds a new rental to the datastore. """
+
         new_rental = {"id": len(self.rentals) + 1, **payload}
         self.rentals.append(new_rental)
         return self.rentals[-1]
 
     def delete_rental(self, id):
+        """ Deletes a rental from the datastore by its ID. """
+
         for rental in self.rentals:
             if rental["id"] == id:
                 store["rentals"] = [
@@ -35,6 +55,7 @@ class RentalService:
         return False
 
     def update_rental(self, id, payload):
+        """ Updates the details of an existing rental """
         rental, rentalIdx = self.find_by_id(self.rentals, id)
 
         if rental is None:
--- services/tenant_service.py
+++ services/tenant_service.py
@@ -1,14 +1,33 @@
+"""
+TenantService Module
+
+This module defines the `TenantService` class, which provides methods for managing tenant records. 
+It interacts with a datastore to retrieve tenant information.
+"""
+
 from db import store
 
 
 class TenantService:
+    """
+    A service class for managing tenant records.
+
+    This class provides methods to retrieve all tenants and filter them by rental property.
+    """
+
     def __init__(self):
+        """Initializes the TenantService with tenant data from the datastore."""
+
         self.tenants = store["tenants"]
 
     def get_all_tenants(self):
+        """ Retrieves all tenants from the datastore. """
+
         return self.tenants
 
     def get_by_rental_id(self, rental_id):
+        """ Finds all tenants associated with a specific rental property. """
+
         tenants = [
             t for t in self.tenants if t["rental_id"] == rental_id
         ]
