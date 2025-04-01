42da1cb1e503810c3b7fc60581d67a37b12f3e2d
Luis Neira <lneira41086@gmail.com>
2025-03-31 21:09:20 -0400

    update test_rentals file

--- resources/tests/test_rentals.py
+++ resources/tests/test_rentals.py
@@ -5,7 +5,7 @@ from resources import bp_rentals
 from services import RentalService, TenantService
 
 
-@pytest.fixture
+@pytest.fixture(scope="class")
 def app():
     app = Flask(__name__)
     app.register_blueprint(bp_rentals, url_prefix="/rentals")
@@ -13,68 +13,56 @@ def app():
     return app
 
 
-@pytest.fixture
+@pytest.fixture(scope="class")
 def client(app):
     return app.test_client()
 
 
-def test_get_rentals(client, mocker):
-    mocker.patch.object(RentalService, "get_all", return_value=[
-        {"id": 1, "property_type": "house"},
-        {"id": 2, "property_type": "apartment"}
-    ])
+@pytest.mark.usefixtures("client")
+class TestRentals:
+    def test_get_rentals(self, client, mocker):
+        mocker.patch.object(RentalService, "get_all", return_value=[
+            {"id": 1, "property_type": "house"},
+            {"id": 2, "property_type": "apartment"}
+        ])
 
-    response = client.get("/rentals")
+        response = client.get("/rentals")
+        assert response.status_code == 200
+        assert response.get_json() == [
+            {"id": 1, "property_type": "house"},
+            {"id": 2, "property_type": "apartment"}
+        ]
 
-    assert response.status_code == 200, "status code should be 200"
-    assert response.get_json() == [
-        {"id": 1, "property_type": "house"},
-        {"id": 2, "property_type": "apartment"}
-    ], "should respond with correct payload"
+    def test_create_rental(self, client, mocker):
+        mocker.patch.object(RentalService, "add_one", return_value={
+            "id": 3, "property_type": "house"
+        })
 
+        response = client.post("/rentals", json={"property_type": "house"})
+        assert response.status_code == 201
+        assert response.get_json() == {"id": 3, "property_type": "house"}
 
-def test_create_rental(client, mocker):
-    mocker.patch.object(RentalService, "add_one", return_value={
-        "id": 3, "property_type": "house"
-    })
+    def test_delete_rental_by_id(self, client, mocker):
+        mocker.patch.object(
+            RentalService, "delete_one_by_id", return_value=True)
+        response = client.delete("/rentals/1")
+        assert response.status_code == 200
+        assert response.data == b""
 
-    response = client.post("/rentals", json={"property_type": "house"})
+    def test_update_rental(self, client, mocker):
+        mocker.patch.object(RentalService, "update_one_by_id", return_value=(
+            True, {"id": 1, "property_type": "apartment"}
+        ))
+        response = client.patch(
+            "/rentals/1", json={"property_type": "apartment"})
+        assert response.status_code == 200
+        assert response.get_json() == {"id": 1, "property_type": "apartment"}
 
-    assert response.status_code == 201, "status code should be 201"
-    assert response.get_json() == {
-        "id": 3, "property_type": "house"
-    }, "should respond with correct payload"
-
-
-def test_delete_rental_by_id(client, mocker):
-    mocker.patch.object(RentalService, "delete_one_by_id", return_value=True)
-
-    response = client.delete("/rentals/1")
-
-    assert response.status_code == 200, "status code should be 200"
-    assert response.data == b"", "payload should be an empty string"
-
-
-def test_update_rental(client, mocker):
-    mocker.patch.object(RentalService, "update_one_by_id", return_value=(
-        True, {"id": 1, "property_type": "apartment"}))
-
-    response = client.patch("/rentals/1", json={"property_type": "apartment"})
-
-    assert response.status_code == 200, "status code should be 200"
-    assert response.get_json() == {
-        "id": 1, "property_type": "apartment"
-    }, "should respond with correct payload"
-
-
-def test_get_tenants_by_rental_id(client, mocker):
-    mocker.patch.object(TenantService, "find", return_value=[
-        {"id": 1, "rental_id": 1, "name": "John Doe"}
-    ])
-
-    response = client.get("/rentals/1/tenants")
-
-    assert response.status_code == 200, "status code should be 200"
-    assert response.get_json() == [
-        {"id": 1, "rental_id": 1, "name": "John Doe"}
-    ], "should respond with correct payload"
+    def test_get_tenants_by_rental_id(self, client, mocker):
+        mocker.patch.object(TenantService, "find", return_value=[
+            {"id": 1, "rental_id": 1, "name": "John Doe"}
+        ])
+        response = client.get("/rentals/1/tenants")
+        assert response.status_code == 200
+        assert response.get_json() == [
+            {"id": 1, "rental_id": 1, "name": "John Doe"}]
