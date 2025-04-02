c198ccb528548985927b672397e93b6b785de863
Luis Neira <lneira41086@gmail.com>
2025-04-01 21:33:25 -0400

    stop echo logs

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from flask import Flask
 import pandas as pd
 import json
 from sqlalchemy import create_engine
+import os
+
+db_file = 'real-estate.db'
+echo_logs = not os.path.exists(db_file)
 
 # Load JSON file in pandas dataframe and create SQLite database
 with open("data.json", "r") as file:
     data = json.load(file)
 
 df = pd.DataFrame(data["rentals"])
-engine = create_engine('sqlite:///real-estate.db', echo=True)
+engine = create_engine('sqlite:///real-estate.db', echo=echo_logs)
 df.to_sql('rentals', con=engine, if_exists='replace', index=False)
 
 df2 = pd.DataFrame(data["tenants"])
