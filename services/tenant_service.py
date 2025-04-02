b46f19824f386f25de4f0cbb37ae28a56b692b6d
Luis Neira <lneira41086@gmail.com>
2025-04-02 09:20:28 -0400

    update tenant service

--- services/tenant_service.py
+++ services/tenant_service.py
@@ -5,20 +5,25 @@ This module defines the `TenantService` class, which provides methods for managi
 It interacts with a datastore to retrieve tenant information.
 """
 
-from db import store
+from database import get_db
 
 
 class TenantService:
     """ A service class for managing tenant records. """
 
-    def __init__(self):
-        self.__tenants = store["tenants"]
+    def __init__(self, db=None):
+        self._db = db or get_db()  # Allow passing a mock database
+        self.conn = self._db.conn
+        self.cursor = self._db.cursor
 
     def get_all(self):
-        return self.__tenants
+        query = "SELECT * FROM tenants"
+        rows = self.cursor.execute(query).fetchall()
+        tenants = [dict(row) for row in rows]
+        return tenants
 
     def find(self, key: str, value):
-        # filter tenants by a given key
-        return [
-            t for t in self.__tenants if t.get(key) == value
-        ]
+        query = f"SELECT * FROM tenants WHERE {key} = ?"
+        rows = self.cursor.execute(query, (value, )).fetchall()
+        tenants = [dict(row) for row in rows]
+        return tenants
