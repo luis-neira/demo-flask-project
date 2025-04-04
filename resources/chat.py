6f059ed54f1267028a20d8aaf5d824e0b1fca98c
Luis Neira <lneira41086@gmail.com>
2025-04-04 08:58:57 -0400

    add tinyllama to API

--- resources/chat.py
+++ resources/chat.py
@@ -13,8 +13,8 @@ chat_history = []
 bp = Blueprint('chat', __name__)
 
 
-@bp.post("/")
-def llm_chat():
+@bp.post("/session")
+def llm_chat_session():
     body = request.get_json()
 
     prompt = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise."
@@ -27,6 +27,25 @@ def llm_chat():
     result = llm.invoke(chat_history)
     response = result.content
     chat_history.append(AIMessage(content=response))
+
+    return jsonify({
+        "ai_response": response
+    })
+
+
+@bp.post("/")
+def llm_chat():
+    body = request.get_json()
+
+    prompt = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise."
+
+    messages = [
+        SystemMessage(content=prompt),
+        HumanMessage(content=body["query"])
+    ]
+
+    result = llm.invoke(messages)
+    response = result.content
 
     return jsonify({
         "ai_response": response
